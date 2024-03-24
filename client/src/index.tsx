ef0cac26bff5888831d9e3206cfe9ee8f4cf3305
kushalbhatia169 <kushalbhatia169@gmail.com>
2024-03-24 22:56:28 +0530

    creating ui for memorial

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { configureStore } from "@reduxjs/toolkit";
+import { reducers } from './reducers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// Create the middleware with Thunk
+const middleware: any = [thunk];
+
+// Configure the Redux store
+const store = configureStore({
+  reducer: reducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware<any>().concat(middleware),
+});
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
